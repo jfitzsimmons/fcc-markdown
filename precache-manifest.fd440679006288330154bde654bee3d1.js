d0e9cb49a9585b547d7a5c2f3ea2e3266b7ed1bc
jfitzsimmons <joefitzsimmons@Corinnes-MacBook-Pro.local>
2018-10-08 16:13:48 -0500

    Updates

---
+++ precache-manifest.fd440679006288330154bde654bee3d1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/fcc-markdown/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "a432972f16cbe49ce385",
+    "url": "/fcc-markdown/static/js/runtime~main.a432972f.js"
+  },
+  {
+    "revision": "e4c0e6a407ca1f2ec0a1",
+    "url": "/fcc-markdown/static/js/main.e4c0e6a4.chunk.js"
+  },
+  {
+    "revision": "a0101866c0a900f184cd",
+    "url": "/fcc-markdown/static/js/1.a0101866.chunk.js"
+  },
+  {
+    "revision": "e4c0e6a407ca1f2ec0a1",
+    "url": "/fcc-markdown/static/css/main.293a2420.chunk.css"
+  },
+  {
+    "revision": "38c49cfd9757b4f5e4f3672cd69f83af",
+    "url": "/fcc-markdown/index.html"
+  }
+];
